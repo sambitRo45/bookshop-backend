d61831d1479b0f42a78ec4f8e31673bb32d13ccc
Sambit Hota <sambithota182004@gmail.com>
2026-08-18 16:22:24 +0530

    Update SecurityConfig.java

--- src/main/java/com/bookshop/config/SecurityConfig.java
+++ src/main/java/com/bookshop/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
                         // EVERYTHING ELSE
                         // =========================
 
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
 
                 .addFilterBefore(
